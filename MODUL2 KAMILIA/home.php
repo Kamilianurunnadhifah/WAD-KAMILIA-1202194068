f6b33f28d240f47fb2e8c47d31fccea8ea4ddfa7
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2021-10-27 15:49:51 +0700

    m2

--- MODUL2 KAMILIA/home.php
+++ MODUL2 KAMILIA/home.php
@@ -2,106 +2,113 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <title>Modul2 - Kamilia_1202194068</title>
+    <title>Modul2 - Kamilia_1202194068</title>
 </head>
 <!-- merubah sedikit disuruh bang ardan -->
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08"
+                aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="home.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="booking.php">Booking</a>
-          </li>
-        </ul>
-      </div>
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
 
-  <h4 class="text-center py-3">WELCOME TO ESD VENUE RESERVATION</h4>
-  <div class="container mb-5">
+<h4 class="text-center py-3">WELCOME TO ESD VENUE RESERVATION</h4>
+<div class="container mb-5">
     <div class="bg-dark">
-      <p class="text-center py-2 text-white">Find your best deal for your event, here!</p>
+        <p class="text-center py-2 text-white">Find your best deal for your event, here!</p>
     </div>
 
     <div class="d-flex justify-content-center">
-      <div class="card " style="width: 22rem;">
-        <img src="https://ak-d.tripcdn.com/images/0221k120008a1jddhB6E6_R_800_525.png" class="card-img-top h-75" alt="...">
-        <div class="card-body">
-          <h5 class="card-title" name="type">Nusantara Hall</h5>
-          <p class="card-text text-secondary mb-0">$2000/ Hour</p>
-          <p class="card-text text-secondary">5000 Capacity</p>
-        </div>
-        <ul class="list-group list-group-flush text-center">
-          <li class="list-group-item fw-bold text-success">Free Parking</li>
-          <li class="list-group-item fw-bold text-success">Full AC</li>
-          <li class="list-group-item fw-bold text-success">Cleaning Service</li>
-          <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+        <div class="card " style="width: 22rem;">
+            <img src="https://ak-d.tripcdn.com/images/0221k120008a1jddhB6E6_R_800_525.png" class="card-img-top h-75"
+                 alt="...">
+            <div class="card-body">
+                <h5 class="card-title" name="type">Nusantara Hall</h5>
+                <p class="card-text text-secondary mb-0">$2000/ Hour</p>
+                <p class="card-text text-secondary">5000 Capacity</p>
+            </div>
+            <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item fw-bold text-success">Free Parking</li>
+                <li class="list-group-item fw-bold text-success">Full AC</li>
+                <li class="list-group-item fw-bold text-success">Cleaning Service</li>
+                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
 
-        </ul>
-        <div class="card-footer text-center">
-          <a href="booking.php?type=Nusantara Hall" class="btn btn-primary" type="button">Book Now</a>
+            </ul>
+            <div class="card-footer text-center">
+                <a href="booking.php?type=Nusantara Hall" class="btn btn-primary" type="button">Book Now</a>
 
+            </div>
         </div>
-      </div>
-      <div class="card mx-3" style="width: 22rem;">
-        <img src="https://i2.wp.com/www.infoperbankan.com/wp-content/uploads/2016/07/Menikah-di-Gedung-BKKBN-Halim.jpg?resize=838%2C484" class="card-img-top h-75" alt="...">
-        <div class="card-body">
-          <h5 class="card-title" name="type">Garuda Hall</h5>
-          <p class="card-text text-secondary mb-0">$1000/ Hour</p>
-          <p class="card-text text-secondary">2000 Capacity</p>
-        </div>
-        <ul class="list-group list-group-flush text-center">
-          <li class="list-group-item fw-bold text-success">Free Parking</li>
-          <li class="list-group-item fw-bold text-success">Full AC</li>
-          <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
-          <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+        <div class="card mx-3" style="width: 22rem;">
+            <img src="https://i2.wp.com/www.infoperbankan.com/wp-content/uploads/2016/07/Menikah-di-Gedung-BKKBN-Halim.jpg?resize=838%2C484"
+                 class="card-img-top h-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title" name="type">Garuda Hall</h5>
+                <p class="card-text text-secondary mb-0">$1000/ Hour</p>
+                <p class="card-text text-secondary">2000 Capacity</p>
+            </div>
+            <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item fw-bold text-success">Free Parking</li>
+                <li class="list-group-item fw-bold text-success">Full AC</li>
+                <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
+                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
 
-        </ul>
-        <div class="card-footer text-center">
-          <a href="booking.php?type=Garuda Hall" class="btn btn-primary" type="button">Book Now</a>
+            </ul>
+            <div class="card-footer text-center">
+                <a href="booking.php?type=Garuda Hall" class="btn btn-primary" type="button">Book Now</a>
 
+            </div>
         </div>
-      </div>
-      <div class="card " style="width: 22rem;">
-        <img src="https://my.yamaha.com/en/files/jpg_eis_hall_main_1200x600_59c120f84c2c00a76c8b5bc46f399da8.jpg" class="card-img-top h-75" alt="...">
-        <div class="card-body">
-          <h5 class="card-title" name="type">Gedung Serba Guna</h5>
-          <p class="card-text text-secondary mb-0">$500/ Hour</p>
-          <p class="card-text text-secondary">500 Capacity</p>
-        </div>
-        <ul class="list-group list-group-flush text-center">
-          <li class="list-group-item fw-bold text-danger">No Free Parking</li>
-          <li class="list-group-item fw-bold text-danger">No Full AC</li>
-          <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
-          <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+        <div class="card " style="width: 22rem;">
+            <img src="https://my.yamaha.com/en/files/jpg_eis_hall_main_1200x600_59c120f84c2c00a76c8b5bc46f399da8.jpg"
+                 class="card-img-top h-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title" name="type">Gedung Serba Guna</h5>
+                <p class="card-text text-secondary mb-0">$500/ Hour</p>
+                <p class="card-text text-secondary">500 Capacity</p>
+            </div>
+            <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item fw-bold text-danger">No Free Parking</li>
+                <li class="list-group-item fw-bold text-danger">No Full AC</li>
+                <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
+                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
 
-        </ul>
-        <div class="card-footer text-center">
-          <a href="booking.php?type=Gedung Serba Guna" class="btn btn-primary" type="button">Book Now</a>
+            </ul>
+            <div class="card-footer text-center">
+                <a href="booking.php?type=Gedung Serba Guna" class="btn btn-primary" type="button">Book Now</a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <footer class="footer mt-auto py-3 bg-light">
+</div>
+<footer class="footer mt-auto py-3 bg-light">
     <div class="container text-center">
-      <span class="text-muted">Created by Kamilia_1202194068</span>
+        <span class="text-muted">Created by Kamilia_1202194068</span>
     </div>
-  </footer>
+</footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 </body>
 
--- MODUL2 KAMILIA/my-booking.php
+++ MODUL2 KAMILIA/my-booking.php
@@ -53,91 +53,95 @@ $phone = $_POST['phone'];
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Modul2 - Kamilia_1202194068</title>
 </head>
 <!-- merubah sedikit disuruh bang ardan -->
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="booking.php">Booking</a>
-                    </li>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08"
+                aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="py-3">
+    <h4 class="text-center">Thank yous Kamilia_1202194068 for Reserving</h4>
+    <h4 class="text-center">Please double check your resevation detail</h4>
+</div>
+<div class="container mb-5">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Booking Number</th>
+            <th scope="col">Name</th>
+            <th scope="col">Check-in</th>
+            <th scope="col">Check-out</th>
+            <th scope="col">Building Type</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Service</th>
+            <th scope="col">Total Price</th>
+        </tr>
+        </thead>
+        <tbody class="table-secondary">
+        <tr>
+            <th scope="row">
+                <?php
+                echo(rand(100000000, 999999999));
+                ?>
+            </th>
+            <td><?php echo "$name" ?></td>
+            <td><?php echo "$date" . " " . date(' H:i:s', $time) ?></td>
+            <td><?php echo "$date" . " " . date('H:i:s', $checkout) ?></td>
+            <td><?php echo "$type" ?></td>
+            <td><?php echo "$phone" ?></td>
+            <td>
+                <ul>
+                    <?php
+                    if (isset($ser1)) {
+                        echo "<li>" . $ser1 . "</li>";
+                    }
+
+                    if (isset($ser2)) {
+                        echo "<li>" . $ser2 . "</li>";
+                    }
+
+                    if (isset($ser3)) {
+                        echo "<li>" . $ser3 . "</li>";
+                    }
+                    ?>
+
                 </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="py-3">
-        <h4 class="text-center">Thank yous Kamilia_1202194068 for Reserving</h4>
-        <h4 class="text-center">Please double check your resevation detail</h4>
+            </td>
+            <td>$<?php echo "$p_tot" ?></td>
+
+        </tr>
+
+        </tbody>
+    </table>
+</div>
+<footer class="footer fixed-bottom py-3 bg-light">
+    <div class="container text-center">
+        <span class="text-muted">Created by Kamilia_1202194068</span>
     </div>
-    <div class="container mb-5">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Booking Number</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Check-in</th>
-                    <th scope="col">Check-out</th>
-                    <th scope="col">Building Type</th>
-                    <th scope="col">Phone Number</th>
-                    <th scope="col">Service</th>
-                    <th scope="col">Total Price</th>
-                </tr>
-            </thead>
-            <tbody class="table-secondary">
-                <tr>
-                    <th scope="row">
-                        <?php
-                        echo (rand(100000000, 999999999));
-                        ?>
-                    </th>
-                    <td><?php echo "$name" ?></td>
-                    <td><?php echo "$date" . " " . date(' H:i:s', $time) ?></td>
-                    <td><?php echo "$date" . " " . date('H:i:s', $checkout) ?></td>
-                    <td><?php echo "$type" ?></td>
-                    <td><?php echo "$phone" ?></td>
-                    <td>
-                        <ul>
-                            <?php
-                            if (isset($ser1)) {
-                                echo "<li>" .  $ser1 . "</li>";
-                            }
+</footer>
 
-                            if (isset($ser2)) {
-                                echo "<li>" .  $ser2 . "</li>";
-                            }
-
-                            if (isset($ser3)) {
-                                echo "<li>" .  $ser3 . "</li>";
-                            }
-                            ?>
-
-                        </ul>
-                    </td>
-                    <td>$<?php echo "$p_tot" ?></td>
-
-                </tr>
-
-            </tbody>
-        </table>
-    </div>
-    <footer class="footer fixed-bottom py-3 bg-light">
-        <div class="container text-center">
-            <span class="text-muted">Created by Kamilia_1202194068</span>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 </body>
 
