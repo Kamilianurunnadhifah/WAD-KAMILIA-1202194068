434170821901027385f68d5d0c4ad098690f06fb
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2021-11-24 14:54:22 +0700

    bug

--- MODUL3 KAMILIA/kamilia_edit.php
+++ MODUL3 KAMILIA/kamilia_edit.php
@@ -1,18 +1,18 @@
-<?php 
-	session_start();
-	include('koneksi.php');
-	if(isset($_SESSION['id_buku'])){
-	  $id = $_SESSION['id_buku'];
-	  $judul=$_POST['judul_buku'];
-        $penulis=$_POST['penulis_buku'];
-        $tahun=$_POST['tahun_terbit'];
-        $deskripsi=$_POST['deskripsi'];
-        $bahasa=$_POST['bahasa'];	 
-	  
-		$sql = "update `buku_table` set `judul_buku`='$judul', `penulis_buku`='$penulis',`tahun_terbit`='$tahun',`deskripsi`='$deskripsi',`bahasa`='$bahasa'
-		where `id_buku`='$id'";
-		mysqli_query($kon,$sql);
-		header("Location:kamilia_home.php?notif=editberhasil");
-	  
-	}
+<?php
+session_start();
+include('koneksi.php');
+if (isset($_SESSION['id_buku'])) {
+    $id = $_SESSION['id_buku'];
+    $judul = $_POST['judul_buku'];
+    $penulis = $_POST['penulis_buku'];
+    $tahun = $_POST['tahun_terbit'];
+    $deskripsi = $_POST['deskripsi'];
+    $bahasa = $_POST['bahasa'];
+
+    $sql = "update `buku_table` set `judul_buku`='$judul', `penulis_buku`='$penulis',`tahun_terbit`='$tahun',`deskripsi`='$deskripsi',`bahasa`='$bahasa'
+		where `id_buku`=1";
+    mysqli_query($kon, $sql);
+    header("Location:kamilia_home.php?notif=editberhasil");
+
+}
 ?>
--- MODUL3 KAMILIA/koneksi.php
+++ MODUL3 KAMILIA/koneksi.php
@@ -1,8 +1,9 @@
 <?php
-	$kon = mysqli_connect("localhost","root","","modul3");
+//	$con = mysqli_connect("localhost","root","","modul3");
+$con = mysqli_connect("localhost:3307", "root", "", "modul3");
 
-	//mengecek koneksi
-	if (mysqli_connect_errno()) {
-		echo "koneksi gagal : " .mysqli_connect_errno();
-	}
+//mengecek koneksi
+if (mysqli_connect_errno()) {
+    echo "koneksi gagal : " . mysqli_connect_errno();
+}
 ?>
